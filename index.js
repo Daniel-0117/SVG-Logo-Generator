55e60d23ddbe358c8cb2c95f10c34e506e087ca4
nevaslon0117@gmail.com <nevaslon0117@gmail.com>
2023-05-23 15:47:31 -0700

    Starter-Code

---
+++ index.js
@@ -0,0 +1,9 @@
+//Imports the two NPMs
+const inquirer = require("inquirer");
+const jest = require("jest");
+
+
+const fs = require("fs");
+
+//Imports classes to from shapes.js
+const {Tri, Cir, Squ} = require("./lib/shapes");
